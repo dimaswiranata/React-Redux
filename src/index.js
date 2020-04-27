b72fa40861e83792b2be2bbd0e897639f9ef45ae
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-04-28 00:05:30 +0700

    Dispatching Actions from within the Component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Reducer from './store/Reducer';
+
+const store = createStore(Reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
