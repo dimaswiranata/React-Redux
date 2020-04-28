6a7e98743f9681a4bd22f3b4ab8dae700905751b
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-04-28 20:26:25 +0700

    Combining Multiple Reducers

---
+++ src/store/reducers/Result.js
@@ -0,0 +1,24 @@
+import * as actionTypes from '../Actions';
+
+const initialState = {
+  results: []
+}
+
+const Reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.STORE_RESULT:
+      return {
+        ...state,
+        results: state.results.concat({id: new Date(), value: action.result})
+      }
+    case actionTypes.DELETE_RESULT:
+      const updatedArray = state.results.filter(result => result.id !== action.resultId);
+      return {
+        ...state,
+        results: updatedArray
+      } 
+  }
+  return state;
+};
+
+export default Reducer;
